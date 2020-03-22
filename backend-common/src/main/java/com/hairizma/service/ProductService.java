5e3a8c72e982b1e08d13c9094d43a2561d9f1ebc
Ivan <ivan.achkasov.ua@gmail.com>
2020-03-22 18:37:51 +0200

    Implemented getting product/products by id/ids

--- backend-common/src/main/java/com/hairizma/service/ProductService.java
+++ backend-common/src/main/java/com/hairizma/service/ProductService.java
@@ -2,11 +2,10 @@ package com.hairizma.service;
 
 import com.hairizma.dto.Product;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Set;
 
 @Path("/product")
 @Produces(MediaType.APPLICATION_XML)
@@ -15,5 +14,13 @@ public interface ProductService {
     @GET
     @Path("/all")
     List<Product> getProducts();
+
+    @GET
+    @Path("/byIds")
+    List<Product> getProducts(@QueryParam("ids") Set<Integer> ids);
+
+    @GET
+    @Path("/{id}")
+    Product get(@PathParam("id") int id);
 
 }
--- services-web-app/src/main/java/com/hairizma/service/ProductServiceImpl.java
+++ services-web-app/src/main/java/com/hairizma/service/ProductServiceImpl.java
@@ -3,24 +3,63 @@ package com.hairizma.service;
 import com.hairizma.dto.Product;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    final List<Product> products = new ArrayList<>();
+    {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Айфон");
+        product.setDescription("Лучший телефон в мире");
+        products.add(product);
+
+        product = new Product();
+        product.setId(2);
+        product.setName("Самсунг");
+        product.setDescription("Ну почти лучший");
+        products.add(product);
+
+        product = new Product();
+        product.setId(3);
+        product.setName("Ксиаоми");
+        product.setDescription("Ну так себе");
+        products.add(product);
+
+        product = new Product();
+        product.setId(4);
+        product.setName("Додж");
+        product.setDescription("Говнище а не телефон");
+        products.add(product);
+
+        product = new Product();
+        product.setId(5);
+        product.setName("Мейзу");
+        product.setDescription("Пойдет, но лучше не надо.");
+        products.add(product);
+    }
+
     @Override
     public List<Product> getProducts() {
-        final Product product0 = new Product();
-        product0.setId(4649);
-        product0.setName("nameeee");
-        product0.setDescription("descrrrr");
+        return products;
+    }
 
-        final Product product1 = new Product();
-        product1.setId(2);
-        product1.setName("awdawdaw1");
+    @Override
+    public List<Product> getProducts(Set<Integer> ids) {
+        return products.stream().filter(product -> ids.contains(product.getId())).collect(Collectors.toList());
+    }
 
-        return Arrays.asList(product0, product1);
+    @Override
+    public Product get(final int id) {
+        for(Product product : products) {
+            if(product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
     }
 
 }
